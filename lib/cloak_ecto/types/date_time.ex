c8d33cad41786bbb66545711c658fd616cf75d22
Bram Verburg <bram.verburg@voltone.net>
2020-06-29 17:16:45 +0300

    Support wrapping plaintext in closure

--- lib/cloak_ecto/types/date_time.ex
+++ lib/cloak_ecto/types/date_time.ex
@@ -29,6 +29,10 @@ defmodule Cloak.Ecto.DateTime do
     quote do
       use Cloak.Ecto.Type, unquote(opts)
 
+      def cast(closure) when is_function(closure, 0) do
+        cast(closure.())
+      end
+
       def cast(value), do: Ecto.Type.cast(:utc_datetime, value)
 
       def before_encrypt(value) do
--- test/cloak_ecto/types/naive_date_time_test.exs
+++ test/cloak_ecto/types/naive_date_time_test.exs
@@ -5,6 +5,10 @@ defmodule Cloak.Ecto.NaiveDateTimeTest do
     use Cloak.Ecto.NaiveDateTime, vault: Cloak.Ecto.TestVault
   end
 
+  defmodule ClosureField do
+    use Cloak.Ecto.NaiveDateTime, vault: Cloak.Ecto.TestVault, closure: true
+  end
+
   setup_all do
     atom_map = %{year: 2017, month: 1, day: 5, hour: 12, minute: 0, second: 0}
 
@@ -35,6 +39,13 @@ defmodule Cloak.Ecto.NaiveDateTimeTest do
     assert {:ok, ~N[2017-01-05 12:00:00]} = Field.cast(state[:string_map])
   end
 
+  test ".cast unwraps closures", state do
+    assert {:ok, ~N[2017-01-05 12:00:00]} = Field.cast(fn -> "2017-01-05T12:00:00" end)
+    assert {:ok, ~N[2017-01-05 12:00:00]} = Field.cast(fn -> ~N[2017-01-05 12:00:00] end)
+    assert {:ok, ~N[2017-01-05 12:00:00]} = Field.cast(fn -> state[:atom_map] end)
+    assert {:ok, ~N[2017-01-05 12:00:00]} = Field.cast(fn -> state[:string_map] end)
+  end
+
   test ".dump encrypts the value" do
     {:ok, ciphertext} = Field.dump(~N[2017-01-05 12:00:00])
     assert ciphertext != ~N[2017-01-05 12:00:00]
@@ -44,5 +55,12 @@ defmodule Cloak.Ecto.NaiveDateTimeTest do
   test ".load decrypts an encrypted value" do
     {:ok, ciphertext} = Field.dump(~N[2017-01-05 12:00:00])
     assert {:ok, ~N[2017-01-05 12:00:00]} = Field.load(ciphertext)
+  end
+
+  test ".load with closure option wraps decrypted value" do
+    {:ok, ciphertext} = ClosureField.dump(~N[2017-01-05 12:00:00])
+    assert {:ok, closure} = ClosureField.load(ciphertext)
+    assert is_function(closure, 0)
+    assert ~N[2017-01-05 12:00:00] = closure.()
   end
 end
