f1dd9fce928682c6aef99c9f26dbc2f71a9635bd
Daniel Berkompas <himself@danielberkompas.com>
2020-10-20 11:51:59 -0700

    Bump version to 1.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cloak.Ecto.MixProject do
   def project do
     [
       app: :cloak_ecto,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
